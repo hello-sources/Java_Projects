d4eee8925daf68e2da55597f17975d8e46a7f238
hello-sources <3245849061@qq.com>
2022-03-16 23:53:34 +0800

    Java+thymeleaf+分页功能+关键字查询

---
+++ pro-web/pro12-fruit1.7-thymel/src/com/atguigu/fruit/servlets/UpdateServlet.java
@@ -0,0 +1,42 @@
+package com.atguigu.fruit.servlets;
+
+import com.atguigu.fruit.dao.FruitDAO;
+import com.atguigu.fruit.dao.impl.FruitDAOImpl;
+import com.atguigu.fruit.pojo.Fruit;
+import com.atguigu.myssm.myspringmvc.ViewBaseServlet;
+import com.atguigu.myssm.util.StringUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/update.do")
+public class UpdateServlet extends ViewBaseServlet {
+    private FruitDAO fruitDAO = new FruitDAOImpl();
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.设置编码
+        request.setCharacterEncoding("utf-8");
+
+        //2.获取参数
+        String fidStr = request.getParameter("fid");
+        Integer fid = Integer.parseInt(fidStr);
+        String fname = request.getParameter("fname");
+        String priceStr = request.getParameter("price");
+        int price = Integer.parseInt(priceStr);
+        String fcountStr = request.getParameter("fcount");
+        Integer fcount = Integer.parseInt(fcountStr);
+        String remark = request.getParameter("remark");
+
+        //3.执行更新
+        fruitDAO.updateFruit(new Fruit(fid, fname, price, fcount, remark));
+
+        //4.资源跳转
+        //super.processTemplate("index", request, response);
+        //此处需要重定向，目的是重新给IndexServlet发请求，重新获取fruitList
+        //然后覆盖到session中，这样index.html页面上显示的session信息是新的
+        response.sendRedirect("index");
+    }
+}
